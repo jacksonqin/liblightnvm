d59233a8a57c87a205ed706b06c73235bf24a58b
Simon A. F. Lund <slund@cnexlabs.com>
2017-01-19 17:16:37 +0100

    dev: Adding a device meta_mode

--- src/nvm_dev.c
+++ src/nvm_dev.c
@@ -351,6 +351,8 @@ static int dev_attr_fill(struct nvm_dev *dev)
 	dev->write_naddrs_max = NVM_NADDR_MAX;
 	dev->read_naddrs_max = NVM_NADDR_MAX;
 
+	dev->meta_mode = NVM_META_MODE_NONE;
+
 	return 0;
 }
 
@@ -437,6 +439,28 @@ const struct nvm_geo * nvm_dev_get_geo(struct nvm_dev *dev)
 int nvm_dev_get_pmode(struct nvm_dev *dev)
 {
         return dev->pmode;
+}
+
+int nvm_dev_get_meta_mode(struct nvm_dev *dev)
+{
+	return dev->meta_mode;
+}
+
+int nvm_dev_set_meta_mode(struct nvm_dev *dev, int meta_mode)
+{
+	switch (meta_mode) {
+		case NVM_META_MODE_NONE:
+		case NVM_META_MODE_ALPHA:
+		case NVM_META_MODE_CONST:
+			break;
+		default:
+			errno = EINVAL;
+			return -1;
+	}
+
+	dev->meta_mode = meta_mode;
+
+	return 0;
 }
 
 int nvm_dev_get_erase_naddrs_max(struct nvm_dev *dev)
